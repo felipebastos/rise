de985243905827ec3eabf3e7f0204d344d56cd64
Felipe Bastos Nunes <felipebastos@ifce.edu.br>
2021-07-17 16:51:12 -0300

    Alertas de inconsistência de dados

--- rise/views.py
+++ rise/views.py
@@ -1,11 +1,49 @@
-from players.models import Alliance
+from players.models import Alliance, PlayerStatus, Player
 from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 
+from datetime import date, timedelta
+
 
 def index(request):
-    return render(request, 'rise/index.html')
+    hoje = date.today()
+
+    players_god = Player.objects.filter(alliance__tag='GoD')
+    god_atrasados = 0
+    god_nulos = 0
+    for player in players_god:
+        status = PlayerStatus.objects.filter(player=player).order_by('-data').first()
+        if status:
+            delta = hoje - status.data
+            if delta.days >15:
+                god_atrasados = god_atrasados + 1
+            if status.power == 0:
+                god_nulos = god_nulos + 1
+        else:
+            god_nulos = god_nulos + 1
+    players_bod = Player.objects.filter(alliance__tag='BoD')
+    bod_atrasados = 0
+    bod_nulos = 0
+    for player in players_bod:
+        status = PlayerStatus.objects.filter(player=player).order_by('-data').first()
+        if status:
+            delta = hoje - status.data
+            if delta.days >15:
+                bod_atrasados = bod_atrasados + 1
+            if status.power == 0:
+                bod_nulos = bod_nulos + 1
+        else:
+            bod_nulos = bod_nulos + 1
+
+    context = {
+        'god_antigos': god_atrasados,
+        'god_nulos': god_nulos,
+        'bod_antigos': bod_atrasados,
+        'bod_nulos': bod_nulos,
+    }
+
+    return render(request, 'rise/index.html', context=context)
 
 
 def logar(request):
