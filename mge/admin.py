ab92c8b268f38b2b227230c502bd297ed5d97b55
Felipe Bastos Nunes <felipebastos@ifce.edu.br>
2022-06-27 15:39:03 -0300

    adicionando modelo ao admin

--- mge/admin.py
+++ mge/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import Mge, Ranking, Punido, Inscrito
+from .models import Mge, Ranking, Punido, Inscrito, EventoDePoder
 
 # Register your models here.
 admin.site.register(Mge)
 admin.site.register(Ranking)
 admin.site.register(Punido)
 admin.site.register(Inscrito)
+admin.site.register(EventoDePoder)
