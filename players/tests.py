819d9a7f47954520a569188d8222fad8e8ff94b7
Felipe Bastos Nunes <felipebastos@ifce.edu.br>
2022-07-05 13:15:55 -0300

    criando testes no Django

--- players/tests.py
+++ players/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from .models import Player
 
 # Create your tests here.
+class PlayerTestCase(TestCase):
+    def setUp(self) -> None:
+        Player.objects.create(game_id='111111111', nick='TestPlayer')
+    
+    def test_player_is_searchable_by_id(self):
+        found = Player.objects.filter(game_id='111111111').first()
+
+        self.assertEqual(found.nick, 'TestPlayer')
